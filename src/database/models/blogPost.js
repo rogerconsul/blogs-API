e6576d4efad8c8aeff9eee533b1a9b672c435a7b
Roger Consul <rogerconsul@yahoo.com.br>
2022-07-14 19:01:47 -0300

    Feat: req10

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,28 @@
+const BlogPost = (sequelize, DataTypes) => {
+  /**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ */
+const BlogPost = sequelize.define("BlogPost", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: DataTypes.STRING,
+  content: DataTypes.STRING,
+  userId: {type: DataTypes.STRING},
+  published: DataTypes.DATE,
+  updated: DataTypes.DATE
+},
+{
+  timestamps: false,
+}
+);
+
+BlogPost.associate = (models) => {
+  BlogPost.belongsTo(models.User, {
+    foreignKey: 'userId', as: 'user'
+  })
+}
+
+return BlogPost;
+};
+
+module.exports = BlogPost;
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -13,8 +13,13 @@ const User = (sequelize, DataTypes) => {
   },
   {
     timestamps: false,
+  })
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'posts'
+    })
   }
-  );
 
   return User;
 };
